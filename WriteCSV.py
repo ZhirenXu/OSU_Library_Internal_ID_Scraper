38689d9ec8bf183e95f039a263bba3df82a81e5a
ZhirenXu <xu.3579@buckeyemail.osu.edu>
2020-05-03 00:16:19 -0400

    Bug Fix
    fix an issue that still get output json even when key is wrong

--- WriteCSV.py
+++ WriteCSV.py
@@ -9,17 +9,18 @@ from itertools import zip_longest
 #           output File pointed by user
 def writeCSV(combinedList, outputFile):
     zippedList = []
-    # zip idList in combinedList for output
-    zippedList = zip_longest(*combinedList, fillvalue = '')
-    # write all ids into file
-    print("Writing identifiers into ", outputFile, "...")
+    
+    outFile = open(outputFile, 'w', encoding = 'utf8', newline='')
+    csvWriter = csv.writer(outFile)
+    # zip subList so csvWriter will written in coloum
+    for subList in combinedList:
+        zippedList = zip_longest(subList, fillvalue = '')
+        # write all ids into file
+        print("Writing identifiers into ", outputFile, "...")
     #open file for output
-    try:
-        outFile = open(outputFile, 'w', encoding = 'utf8', newline='')
-        csvWriter = csv.writer(outFile)
-        for element in zippedList:
-            csvWriter.writerow(element)
-        print("Complete!")
-        outFile.close()
-    except:
-        print("Fail to write csv!")
+        try:
+            csvWriter.writerows(zippedList)
+        except:
+            print("Fail to write csv!")
+    print("Complete!")
+    outFile.close()
