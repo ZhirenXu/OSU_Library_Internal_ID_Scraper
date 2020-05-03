5517840c824a3509b9ac93041730f1b4bd2e4107
ZhirenXu <xu.3579@buckeyemail.osu.edu>
2020-05-03 00:19:07 -0400

    Revert "Bug Fix"

--- WriteCSV.py
+++ WriteCSV.py
@@ -9,18 +9,17 @@ from itertools import zip_longest
 #           output File pointed by user
 def writeCSV(combinedList, outputFile):
     zippedList = []
-    
-    outFile = open(outputFile, 'w', encoding = 'utf8', newline='')
-    csvWriter = csv.writer(outFile)
-    # zip subList so csvWriter will written in coloum
-    for subList in combinedList:
-        zippedList = zip_longest(subList, fillvalue = '')
-        # write all ids into file
-        print("Writing identifiers into ", outputFile, "...")
+    # zip idList in combinedList for output
+    zippedList = zip_longest(*combinedList, fillvalue = '')
+    # write all ids into file
+    print("Writing identifiers into ", outputFile, "...")
     #open file for output
-        try:
-            csvWriter.writerows(zippedList)
-        except:
-            print("Fail to write csv!")
-    print("Complete!")
-    outFile.close()
+    try:
+        outFile = open(outputFile, 'w', encoding = 'utf8', newline='')
+        csvWriter = csv.writer(outFile)
+        for element in zippedList:
+            csvWriter.writerow(element)
+        print("Complete!")
+        outFile.close()
+    except:
+        print("Fail to write csv!")
