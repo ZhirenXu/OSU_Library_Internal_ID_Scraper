2e60a2110b127f7f468269a4146e42064ebdfd38
ZhirenXu <xu.3579@buckeyemail.osu.edu>
2020-05-03 00:08:15 -0400

    Code Clean up, Bug Fix

---
+++ Category_list.py
@@ -0,0 +1,4 @@
+categoryList = ["f[admin_set_sim][]", "f[collection_name_sim][]", "f[sub_collection_sim][]",
+                "f[resource_type_sim][]", "f[creator_sim][]", "f[keyword_sim][]", "f[subject_sim][]",
+                "f[format_sim][]", "f[work_type_sim][]", "f[language_sim][]", "f[spatial_sim][]",
+                "f[publisher_sim][]", "f[temporal_sim][]"]
